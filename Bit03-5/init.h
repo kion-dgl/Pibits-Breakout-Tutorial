f1a9030b890917110a14347f7c25006c9ff4e877
kion <kion@dashgl.com>
2024-10-07 04:19:38 -0500

    bounce ball

---
+++ Bit03-5/init.h
@@ -0,0 +1,21 @@
+#ifndef INIT_H
+#define INIT_H
+
+#include <SDL2/SDL.h>
+#include <GL/glew.h>
+
+// Function declarations for initialization
+
+typedef struct {
+    GLuint VBO;         // Vertex Buffer Object
+    GLuint textureID;   // Texture ID
+    float radius;       // Radius of the ball
+    float x, y;         // Position of the ball
+    float vx, vy;       // Velocity of the ball
+} Ball;
+
+
+int initializeSDLAndOpenGL(SDL_Window** window, SDL_GLContext* glContext, GLuint* shaderProgram);
+void initBall(Ball* ball, GLuint shaderProgram);
+
+#endif // INIT_H
